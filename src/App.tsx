6b37c7670c30992802a83004deb0b189535172cf
DashaBaltsevich <dasha.baltsevich@mail.ru>
2022-06-29 16:37:11 +0300

    Firstscreen add

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { FirstScreen } from './components';
+import './App.css';
+import React from 'react';
+
+function App () {
+    return (
+        <FirstScreen />
+    )
+}
+
+export default App;
---
+++ src/components/FirstScreen/FirstScreen.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './FirstScreen.scss';
+
+export const FirstScreen: React.FC = () => {
+    return (        
+        <nav className="first-screen__nav">
+            <ul className="l-first-screen">
+                <li className="l-first-screen__item">
+                    <NavLink to="/login">
+                        Найти специалиста
+                    </NavLink>
+                </li>
+                <li className="l-first-screen__item">
+                    <NavLink to="/login">
+                        Вход для специалистов
+                    </NavLink>
+                </li>
+            </ul>     
+        </nav>
+    )
+}
