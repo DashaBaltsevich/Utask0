17cd493aa4e727544dbbfb09b47452c457cece2b
DashaBaltsevich <dasha.baltsevich@mail.ru>
2022-06-30 11:30:26 +0300

    loginForm add, formik, yup install

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import { FirstScreen } from "./components";
-import "./App.css";
-import React from "react";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { FirstScreen, LoginForm } from './components';
+import './App.css';
 
 function App() {
-  return <FirstScreen />;
+  return (
+    <Routes>
+      <Route path="/" element={<FirstScreen />} />
+      <Route path="login" element={<LoginForm />} />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/components/LoginForm/LoginForm.tsx
@@ -0,0 +1,94 @@
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { NavLink } from 'react-router-dom';
+import './LoginForm.scss';
+import * as yup from 'yup';
+import React from 'react';
+
+const validationSchema = yup.object({
+  email: yup
+    .string()
+    .email('Invalid email address')
+    .required('Email is required')
+    .max(30, 'Are you really sure that your email is that big?'),
+  password: yup
+    .string()
+    .required('Password is required')
+    .min(4, 'Must be more than 4 characters'),
+});
+
+export const LoginForm = () => {
+  const handleFormSubmit = (values: object) => {
+    console.log(values);
+  };
+  return (
+    <section className="s-login">
+      <Formik
+        initialValues={{
+          email: '',
+          password: '',
+        }}
+        validateOnBlur={false}
+        validationSchema={validationSchema}
+        onSubmit={(values) => handleFormSubmit(values)}
+      >
+        {({ values }) => (
+          <Form className="f-login">
+            <h1 className="f-login__title">Utask</h1>
+            <div className="f-login__row">
+              <label htmlFor="email" className="f-login__field-label">
+                Почта:
+              </label>
+              <br />
+              <Field
+                type="email"
+                id="email"
+                name="email"
+                className="f-login__field"
+                value={values.email}
+                placeholder="Почта"
+              />
+              <ErrorMessage
+                name="email"
+                component={() => <p className="f-login__field-error"></p>}
+              />
+            </div>
+
+            <div className="f-login__row">
+              <label htmlFor="password" className="f-login__field-label">
+                Пароль:
+              </label>
+              <br />
+              <Field
+                type="password"
+                id="password"
+                name="password"
+                className="f-login__field"
+                placeholder="Пароль"
+              />
+              <ErrorMessage
+                name="password"
+                component={() => <p className="f-login__field-error"></p>}
+              />
+            </div>
+
+            <button type="submit" className="f-login__btn-submit">
+              Войти
+            </button>
+
+            <div className="f-login__row-links">
+              <NavLink
+                to="/registration"
+                className="f-login__link-registration"
+              >
+                Зарегестрироваться
+              </NavLink>
+              <NavLink to="/password" className="f-login__link-forgot-password">
+                Забыли пароль?
+              </NavLink>
+            </div>
+          </Form>
+        )}
+      </Formik>
+    </section>
+  );
+};
